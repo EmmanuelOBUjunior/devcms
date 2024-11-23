a513853fac54a6fc1b2a37ed2e106c65e487aea3
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-23 18:47:34 +0000

    Cleaning up devcms

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@
 export default function Home() {
   console.log("What am I doing here?")
   return (
-    <h1 className="text-3xl">Welcome to nextjs</h1>
+    <main>
+      <h1 className="text-3xl">Welcome to nextjs</h1>
+      <p>This is the list of users:</p>
+    </main>
   );
 }
