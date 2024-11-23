3879da11038107e87bec52967165c1743c205402
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-23 18:56:03 +0000

    Cleaning up devcms

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   console.log("What am I doing here?")
   return (
     <main>
       <h1 className="text-3xl">Welcome to nextjs</h1>
       <p>This is the list of users:</p>
+      <ul>
+        <li><Link href={`/userprofile/1`}>This is for user 01</Link></li>
+      </ul>
     </main>
   );
 }
--- app/userprofile/[id]/page.tsx
+++ app/userprofile/[id]/page.tsx
@@ -3,7 +3,7 @@ const UserProfile = ({params}:{params:{id:string}}) => {
     const {id} = params
   return (
     <div>
-      <p>User Profile: {id}</p>
+      <h1>User Profile: {id}</h1>
     </div>
   )
 }
