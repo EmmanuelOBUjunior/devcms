c12dd6aadf90b90484fc738c8fe29f771efce00d
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-30 19:00:21 +0000

    Working on the search component

---
+++ app/components/SearchFormReset.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SearchFormReset = () => {
+    const reset = () => {
+        const form = document.querySelector('.search-form') as HTMLFormElement
+
+    }
+  return (
+    <button type='reset' onClick={reset}></button>
+  )
+}
+
+export default SearchFormReset
