e53b75c45c0a53cd4330b216550c55a5110834ec
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-30 16:50:34 +0000

    Working on the navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,16 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
     <header className='px-6 py-3 bg-white shadow-sm font-work-sans'>
       <nav className='flex justify-between items center'>
-
+        <Link href='/'>
+        <Image
+        
+        />
+        </Link>
       </nav>
     </header>
   )
