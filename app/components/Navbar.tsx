a30c0604a47cf7dcf4e4b5d292e61931787fa828
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-30 16:59:08 +0000

    working on the navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,22 +1,17 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
   return (
-    <header className='px-6 py-3 bg-white shadow-sm font-work-sans'>
-      <nav className='flex justify-between items center'>
-        <Link href='/'>
-        <Image
-        src='/yclogo.png'
-        alt='Logo'
-        width={144}
-        height={30}
-        />
+    <header className="px-6 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items center">
+        <Link href="/">
+          <Image src="/yclogo.png" alt="Logo" width={144} height={30} />
         </Link>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
