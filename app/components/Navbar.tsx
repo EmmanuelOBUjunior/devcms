06ee20bf6266dbfd842655607a007b0a7380d50d
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-30 17:09:01 +0000

    working on the navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Navbar = async() => {
-  const session = await auth()
+const Navbar = async () => {
+  const session = await auth();
   return (
     <header className="px-6 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items center">
@@ -15,16 +15,18 @@ const Navbar = async() => {
         <div className="flex items-center gap-5">
           {session && session?.user ? (
             <>
-            <Link href='/startup/create'><span>Create</span></Link>
-            <button onClick={signOut}>
-              <span>Sign Out</span>
-            </button>
-            <Link href={`/user/${session?.id}`}>
-            <span>{session?.user.name}</span>
-            </Link>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <button onClick={signOut}>
+                <span>Sign Out</span>
+              </button>
+              <Link href={`/user/${session?.id}`}>
+                <span>{session?.user.name}</span>
+              </Link>
             </>
-          ):(
-            <button onClick={signIn('github')}>
+          ) : (
+            <button onClick={signIn("github")}>
               <span>Login</span>
             </button>
           )}
