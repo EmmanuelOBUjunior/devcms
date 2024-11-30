7b5e85a3b3aa091ef3bff3651afdb1c688784ff5
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-30 16:49:52 +0000

    Working on the navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <header>
-      Navbar
+    <header className='px-6 py-3 bg-white shadow-sm font-work-sans'>
+      <nav className='flex justify-between items center'>
+
+      </nav>
     </header>
   )
 }
