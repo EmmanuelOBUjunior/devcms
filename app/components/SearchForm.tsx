4650e0baaa8e30af0b512de5bda30a7549f8e6e0
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-30 18:56:00 +0000

    Working on the search component

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import Form from 'next/form'
 
 const SearchForm = () => {
+    const query = 'Test'
+
+    const reset = () => {
+        
+    }
+
   return (
-    <div>SearchForm</div>
+    <Form className='search-form' action='/'>
+        <input type="text"
+        placeholder='Search Startup'
+        name='query'
+        className='search-input'
+        defaultValue={query}
+        />
+
+        <div className='flex gap-2'>
+            {query && (
+                <button onClick={reset}></button>
+            )}
+        </div>
+    </Form>
   )
 }
 
