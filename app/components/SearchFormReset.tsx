65ff2f9eddabb99edb26d77eec12c2342b0d1889
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-11-30 19:02:03 +0000

    Working on the search component

--- app/components/SearchFormReset.tsx
+++ app/components/SearchFormReset.tsx
@@ -1,11 +1,16 @@
 'use client'
+
+import Link from "next/link"
+
 const SearchFormReset = () => {
     const reset = () => {
         const form = document.querySelector('.search-form') as HTMLFormElement
 
     }
   return (
-    <button type='reset' onClick={reset}></button>
+    <button type='reset' onClick={reset}>
+        <Link href='/'className="search-btn text-white">X</Link>
+    </button>
   )
 }
 
