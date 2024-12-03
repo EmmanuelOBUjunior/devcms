f2643d13e1a928d71edc8aecb7dc474215fef8a0
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-12-03 22:32:00 +0000

    Working on the list of startups section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,17 @@ import SearchForm from "../../components/SearchForm";
 
 export default async function Home({searchParams}:{searchParams:Promise<{query?:string}>}) {
   const query = (await searchParams).query
+
+  const post = [{
+    _createdAt: 'Yesterday',
+    views: 55,
+    author: {_id: 1},
+    description: 'This is the description of the post',
+    image: 'https://plus.unsplash.com/premium_photo-1677094310899-02303289cadf?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cm9ib3R8ZW58MHx8MHx8fDA%3D',
+    category: 'Robots',
+    title: 'We Robots',
+  }]
+
   return (
     <>
     <section className="pink_container">
@@ -13,8 +24,10 @@ export default async function Home({searchParams}:{searchParams:Promise<{query?:
     <p className="text-30-semibold">
       {query ? `Search results for ${query}`:'All Startups'}
     </p>
-    <ul className="mt-7 card-grid">
-      
+    <ul className="mt-7 card_grid">
+      {post.length > 0 ? (
+        post.map((post:StartupCardType, index:number)=>(<StartupCard/>))
+      ):(<p className="no-results">No startup found</p>)}
     </ul>
     </section>
     </>
