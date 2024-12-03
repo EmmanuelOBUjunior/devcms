3eb3f520ec4fb8c03a8240e576902b5536a6296c
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-12-03 23:56:19 +0000

    feat: working on the startupcard

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,5 +1,6 @@
 import { formatDate } from "@/lib/utils";
 import { EyeIcon } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
 const StartupCard = ({ post }: { post: StartupCardtype }) => {
@@ -7,20 +8,26 @@ const StartupCard = ({ post }: { post: StartupCardtype }) => {
   return (
     <li className="startup-card group">
       <div className="flex-between">
-        <p className="startup_card_date">{formatDate(post._createdAt)}</p>
+        <p className="startup_card_date">{formatDate(_createdAt)}</p>
 
         <div className="flex gap-1.5">
           <EyeIcon className="size-6 text-primary" />
-          <span className="text-16-medium">{post.views}</span>
+          <span className="text-16-medium">{views}</span>
         </div>
       </div>
 
       <div className="flex-between mt-5 gap-5">
         <div className="flex-1">
-          <Link href={`/user/${post.author?._id}`}>
-            <p className="text-16-medium line-clamp-1">{post.author?.name}</p>
+          <Link href={`/user/${authorId}`}>
+            <p className="text-16-medium line-clamp-1">{name}</p>
+          </Link>
+          <Link href={`/startups/${_id}`}>
+            <p className="text-26-semibold line-clamp-1">{title}</p>
           </Link>
         </div>
+        <Link href={`/user/${authorId}`}>
+        <Image src='https://placehold.co/600*400' alt="placeholder" width={48} height={48} className="rounded=full"/>
+        </Link>
       </div>
     </li>
   );
