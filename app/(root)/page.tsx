7a7b591849be6a37377faf12a354ce955ab28dff
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-12-03 21:26:02 +0000

    feat: working on the search params

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 import SearchForm from "../components/SearchForm";
 
-export default function Home() {
-
+export default async function Home({searchParams}:{searchParams:Promise<{query?:string}>}) {
+  const query = (await searchParams).query
   return (
     <>
     <section className="pink_container">
