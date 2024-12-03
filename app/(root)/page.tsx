079ff146c332b5d878b0dc170850ce7d22d0e25a
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-12-03 22:19:55 +0000

    Working on the list of startups section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,6 +9,11 @@ export default async function Home({searchParams}:{searchParams:Promise<{query?:
       <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions</p>
       <SearchForm query = {query}/>
     </section>
+    <section className="section_container">
+    <p className="text-30-bold">
+      {query ? `Search results for ${query}`:'All Startups'}
+    </p>
+    </section>
     </>
   );
 }
