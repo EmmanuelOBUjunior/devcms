aae5f70bd60c1a84252181f4238b7eeaee387d97
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-12-06 12:19:49 +0000

    working on the query for startups

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,15 +9,7 @@ export default async function Home({searchParams}:{searchParams:Promise<{query?:
   const posts = await client.fetch(STARTUP_QUERY)
   console.log(JSON.stringify(posts))
 
-  // const posts = [{
-  //   _createdAt: new Date(),
-  //   views: 55,
-  //   author: {_id: 1, name: 'Emmanuel Junior'},
-  //   description: 'This is the description of the post',
-  //   image: 'https://plus.unsplash.com/premium_photo-1677094310899-02303289cadf?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cm9ib3R8ZW58MHx8MHx8fDA%3D',
-  //   category: 'Robots',
-  //   title: 'We Robots',
-  // }]
+
 
   return (
     <>
