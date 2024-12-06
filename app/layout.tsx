b798b6b49f2e6f6b57819eb798befc05c3658b69
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-12-06 11:44:43 +0000

    Added sanity markdown

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import 'easydme/dist/easydme.min.css'
 
 const workSans = localFont({
   src:[
