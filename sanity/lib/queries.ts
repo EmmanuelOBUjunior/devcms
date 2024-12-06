449772e8e89aace2e5c536e6e1eedbac01d39d4f
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-12-06 12:15:01 +0000

    working on the query for startups

---
+++ sanity/lib/queries.ts
@@ -0,0 +1,18 @@
+import { defineQuery } from "next-sanity";
+
+export const STARTUP_QUERY = defineQuery(`*[_type == 'startup' && defined(slug.current)]{
+  _id,
+  title,
+  category,
+  views,
+  _createdAt,
+  description,
+  slug,
+  author -> {
+    _id,
+    name,
+    image,
+    bio
+  },
+  image
+}`)
