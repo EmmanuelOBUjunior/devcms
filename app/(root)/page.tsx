ab1b87ef6d1d8927b77dff4f3960bd4f702e8b49
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-12-06 13:23:57 +0000

    Working on the types for startupcard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,31 +3,40 @@ import SearchForm from "../../components/SearchForm";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_QUERY } from "@/sanity/lib/queries";
 
-export default async function Home({searchParams}:{searchParams:Promise<{query?:string}>}) {
-  const query = (await searchParams).query
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
 
-  const posts = await client.fetch(STARTUP_QUERY)
+  const posts = await client.fetch(STARTUP_QUERY);
   // console.log(JSON.stringify(posts))
-
-
 
   return (
     <>
-    <section className="pink_container">
-      <h1 className="heading">Pitch Your Startup, <br /> Connect with entrepreneurs</h1>
-      <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions</p>
-      <SearchForm query = {query}/>
-    </section>
-    <section className="section_container">
-    <p className="text-30-semibold">
-      {query ? `Search results for ${query}`:'All Startups'}
-    </p>
-    <ul className="mt-7 card_grid">
-      {posts.length > 0 ? (
-        posts.map((post: StartupCardType)=>(<StartupCard key={post._id} post={post}/>))
-      ):(<p className="no-results">No startup found</p>)}
-    </ul>
-    </section>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect with entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+        <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for ${query}` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts.length > 0 ? (
+            posts.map((post) =>( 
+            <StartupCard key={post._id} post={post} />))
+          ) : (
+            <p className="no-results">No startup found</p>
+          )}
+        </ul>
+      </section>
     </>
   );
 }
