18fe1c639e7d9031a244a3e2d7d459a0b7145104
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-12-15 18:02:40 +0000

    feat:working on the individual startup page

--- app/(root)/startups/[id]/page.tsx
+++ app/(root)/startups/[id]/page.tsx
@@ -4,7 +4,9 @@ const Page = async ({params}: {params:Promise<{id:string}>}) => {
   const id = (await params).id
 
   return (
-    <div>Startup</div>
+    <>
+    <h1 className='text-3xl'>This is a startup number: ${id}</h1>
+    </>
   )
 }
 
