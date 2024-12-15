e1822ba817ead279a7495fbb6733fc3d5681b81e
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-12-15 20:15:33 +0000

    feat: working on the inidividual startup page

--- app/(root)/startups/[id]/page.tsx
+++ app/(root)/startups/[id]/page.tsx
@@ -29,7 +29,10 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
         />
         <div className="space-y-5 mt-10 max-w-4xl mx-auto">
           <div className="flex-between gap-5">
-            <Link href={`/user/${post?.author?._id}`} className="flex gap-2 items-center mb-3">
+            <Link
+              href={`/user/${post?.author?._id}`}
+              className="flex gap-2 items-center mb-3"
+            >
               <Image
                 src={post.author?.image}
                 alt="avatar"
@@ -39,7 +42,9 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
               />
               <div>
                 <p className="text-20-medium">{post.author?.name}</p>
-                <p className="text-16-medium !text-black-300">@{post.author?.username}</p>
+                <p className="text-16-medium !text-black-300">
+                  @{post.author?.username}
+                </p>
               </div>
             </Link>
           </div>
