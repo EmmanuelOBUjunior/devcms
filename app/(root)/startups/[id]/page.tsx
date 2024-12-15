c5370a3bd8aff5cff751f1878f21c751eb7fd771
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-12-15 19:56:04 +0000

    feat: working on the inidividual startup page

--- app/(root)/startups/[id]/page.tsx
+++ app/(root)/startups/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { formatDate } from '@/lib/utils'
 import { client } from '@/sanity/lib/client'
 import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries'
 import { notFound } from 'next/navigation'
@@ -13,7 +14,11 @@ const Page = async ({params}: {params:Promise<{id:string}>}) => {
 
   return (
     <>
-    <section className='pink_container !min-h-[230px]'></section>
+    <section className='pink_container !min-h-[230px]'>
+      <p className='tag'>{formatDate(post?._createdAt)}</p>
+      <h1 className='heading'>{post.title}</h1>
+      <p className='sub-heading !max-w-5xl'>{post.description}</p>
+    </section>
     </>
   )
 }
