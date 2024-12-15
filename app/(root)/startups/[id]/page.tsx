1325875b11ed207bca88351b73feefc082639320
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2024-12-15 19:51:54 +0000

    feat: working on the inidividual startup page

--- app/(root)/startups/[id]/page.tsx
+++ app/(root)/startups/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { client } from '@/sanity/lib/client'
 import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries'
+import { notFound } from 'next/navigation'
 import React from 'react'
 
 export const experimental_ppr = true
@@ -7,6 +8,8 @@ export const experimental_ppr = true
 const Page = async ({params}: {params:Promise<{id:string}>}) => {
   const id = (await params).id
   const post = await client.fetch(STARTUP_BY_ID_QUERY, {id})
+
+  if(!post) return notFound()
 
   return (
     <>
